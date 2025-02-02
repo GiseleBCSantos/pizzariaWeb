54b3f514d4b05d32f9b9b94d4a385548cedc4e3f
GiseleBCSantos <catce.2024111mtds0035@aluno.ifpi.edu.br>
2025-02-02 09:04:08 -0300

    :sparkles: Add Provider e toastContainer no App

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
+import { PizzariaProvider } from "./modules/views/pizzaria-provider.tsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <PizzariaProvider>
+      <ToastContainer position="top-center" autoClose={3000} />
+      <App />
+    </PizzariaProvider>
   </StrictMode>
 );
